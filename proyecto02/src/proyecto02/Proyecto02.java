38b2cafb32da750c6ddbba9cebcf64dfb8a32b5c
Javier Vinan <javinan5@utpl.edu.ec>
2024-10-26 19:51:31 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author javiv
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos basico de Java, Versionado a traves de Git, Manejado desde GitHub, Ademas se usar GitHubClassroom");
+        // TODO code application logic here
+    }
+    
+}
