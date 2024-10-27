bce6f92d3e8cf3cb6547a62613d2233fb005eba2
Javier Vinan <javinan5@utpl.edu.ec>
2024-10-26 19:46:56 -0500

    segundo commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -14,7 +14,7 @@ public class Proyecto01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Ejemplo basico de Java Versionado a traves de Git Manejado desde GitHub");
+        System.out.println("Ejemplo de Java, manejado desde GitHub, versionado a traves de Git, creado por ubicar su usuario de github");
         // TODO code application logic here
     }
     
